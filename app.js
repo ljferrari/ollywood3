4fb2f129c6053a6b2371f8411968d73c3e072abd
LjayeFerrari <luciennejferrari@gmail.com>
2017-08-07 11:18:59 +0100

    removed fixed bg for mobile, aded skrollr

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+
+
+
+
 //$(window).scroll(function(){
 //    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
 //  });
+
+
+
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+
+alert("I am an alert box!");
